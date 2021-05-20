82cee9c72c774e68e1adda26e348b5a9bdbb7177
CronoxSoft Mac <josafatfloresr@gmail.com>
2021-05-19 19:44:09 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Josafat Flores on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
